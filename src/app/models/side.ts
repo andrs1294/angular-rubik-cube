fcb171a260f8510ab956350902dfa1787a2379c8
Andrés M. Gómez <andres86mauricio@hotmail.com>
2019-02-16 10:21:57 -0500

    Se modifica la descripción del stack

--- src/app/models/side.ts
+++ src/app/models/side.ts
@@ -10,8 +10,8 @@ export class Side {
   constructor(public color: Color, public position: SidePosition, public text: string = '') {
     this.cells = [
       [new Cell(color, this.getValueJson(text,0)), new Cell(color, this.getValueJson(text,1)), new Cell(color, this.getValueJson(text,2))],
-      [new Cell(color, this.getValueJson(text,4)), new Cell(color, this.getValueJson(text,5)), new Cell(color, this.getValueJson(text,6))],
-      [new Cell(color, this.getValueJson(text,8)), new Cell(color, this.getValueJson(text,9)), new Cell(color, this.getValueJson(text,10))],
+      [new Cell(color, this.getValueJson(text,3)), new Cell(color, this.getValueJson(text,4)), new Cell(color, this.getValueJson(text,5))],
+      [new Cell(color, this.getValueJson(text,6)), new Cell(color, this.getValueJson(text,7)), new Cell(color, this.getValueJson(text,8))],
     ];
   }
 
